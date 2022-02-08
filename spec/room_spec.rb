4a89666db3ee0a5e39072f75856dcbe1322f5b77
Chris Simmons <chris@turing.edu>
2022-02-08 16:21:50 -0500

    feat: initial student setup

---
+++ spec/room_spec.rb
@@ -0,0 +1,22 @@
+require './lib/room'
+
+RSpec.describe Room do
+  context "Iteration 1" do
+    it "exists" do
+      room = Room.new(:bedroom, 10, '13')
+      expect(room).to be_an_instance_of(Room)
+    end
+
+    it "has a category" do
+      room = Room.new(:bedroom, 10, '13')
+      expect(room.category).to eq(:bedroom)
+    end
+
+    it "can get area" do
+      room1 = Room.new(:bedroom, 10, '13')
+      room2 = Room.new(:living_room, 15, '12')
+      expect(room1.area).to eq(130)
+      expect(room2.area).to eq(180)
+    end
+  end
+end
