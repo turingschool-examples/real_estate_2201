8e1765c300e545130ad4cf434eb77e19167e106b
Chris Simmons <chris@turing.edu>
2022-02-08 16:27:36 -0500

    fix: skipping tests

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Room do
       expect(room).to be_an_instance_of(Room)
     end
 
-    it "has a category" do
+    xit "has a category" do
       room = Room.new(:bedroom, 10, '13')
       expect(room.category).to eq(:bedroom)
     end
 
-    it "can get area" do
+    xit "can get area" do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
       expect(room1.area).to eq(130)
