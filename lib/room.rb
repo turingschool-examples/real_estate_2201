b3ff10ba69ae92baac30f2f9ee5e0f0d57f3f1ee
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-09 11:09:04 -0500

    Initial tests pass.

---
+++ lib/room.rb
@@ -0,0 +1,14 @@
+class Room
+  attr_reader :category, :length, :width
+    
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+  end
+
+  def area
+    return length * width.to_i
+  end
+    
+end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Room do
       expect(room).to be_an_instance_of(Room)
     end
 
-    xit "has a category" do
+    it "has a category" do
       room = Room.new(:bedroom, 10, '13')
       expect(room.category).to eq(:bedroom)
     end
 
-    xit "can get area" do
+    it "can get area" do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
       expect(room1.area).to eq(130)
