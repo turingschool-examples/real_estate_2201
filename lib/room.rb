d75d762d099dc1417ac0aa9bdd216715dc44f97b
“Brad <hutch13531@gmail.com>
2022-02-09 12:58:03 -0500

    added room class

---
+++ lib/room.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Room
+  attr_reader :category, :length, :width
+  def initialize(category, length, width)
+    @room = room
+    @category = category
+    @length = length
+    @width = width
+    @room_area = []
+    @paint = paint
+  end
+
+  def interior(area)
+    room.push(legth * width)<<[]
+  end
+
+  def is_painted?(paint)
+    if is_painted == true
+      puts true
+    else if (room.paint)!= false
+      puts false
+
+    end
+  end 
+  # binding.pry
+end
