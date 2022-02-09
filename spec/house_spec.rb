ec4c9e752d6cde8a559d2a505e5dca962110c3bc
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-09 09:55:54 -0700

    Test: create test for rooms_from_category method

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -39,4 +39,18 @@ RSpec.describe House do
       expect(house.above_market_average?).to eq(false)
     end
   end
+
+  describe 'rooms_from_category' do
+    it 'returns an array of rooms from a given category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2, room_4])
+      expect(house.rooms_from_category(:basement)).to eq([room3])
+    end
+  end
+
 end
