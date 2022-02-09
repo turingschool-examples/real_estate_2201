2c636fc1453f6326ea4327dfc24281fc0903f415
phdd0 <paul.haddick@outlook.com>
2022-02-09 10:57:03 -0700

    add: to House, see i need to pass method > method for add_room

--- lib/house.rb
+++ lib/house.rb
@@ -1,10 +1,21 @@
 class House
 
-  attr_reader :price, :address
+  attr_reader :price, :address, :rooms
 
-  def initialize(
-    @price = "$400000"
-    @address = "123 sugar lane"
+  def initialize(p, a)
+    @price = p
+    @address = a
+    @rooms = []
+  end
+
+  def price
+    @price.to_f
+  end
+
+  def add_room
+
+    
+
   end
 
 end
