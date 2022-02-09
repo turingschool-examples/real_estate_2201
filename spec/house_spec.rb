1e36eeff6acb7ea1edee495c63e1b6c7c823c87d
Luke Pascale <lpluke5@gmail.com>
2022-02-09 09:48:44 -0700

    feat: Add ability to add rooms to the house class.

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe House do
 
       expect(house).to be_a(House)
       expect(house.price).to eq(400000)
+      expect(house.address).to eq("123 sugar lane")
+      expect(house.rooms).to eq([])
+    end
+
+    it 'can have different rooms' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+      expect(house.rooms).to eq([room_1, room_2])
     end
   end
 end
