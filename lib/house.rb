5f42e92f5ea45451745d786456e9781120396c31
Joseph Downs <jos.w.downs@gmail.com>
2022-02-09 10:56:03 -0700

    Add sorting hat

--- lib/house.rb
+++ lib/house.rb
@@ -2,13 +2,13 @@ require "./room"
 require "pry"
 
 class House
-attr_reader :price, :address, :rooms, :organize, :total_area, :details
+attr_reader :price, :address, :rooms, :organize_by_category, :total_area, :details
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
-    @organize = []
+    @organize_by_category = []
     @total_area = 0
     @details = {"price" => price.to_i, "address" => address}
   end
@@ -26,16 +26,16 @@ attr_reader :price, :address, :rooms, :organize, :total_area, :details
   end
 
   def rooms_from_category(gory)
-    until @organize == [] do
-      @organize.shift
+    until @organize_by_category == [] do
+      @organize_by_category.shift
     end
     @rooms.each do |cat|
       if gory == cat.category
-        @organize << cat
+        @organize_by_category << cat
       else
       end
     end
-    return @organize
+    return @organize_by_category
   end
 
   def area
@@ -43,6 +43,15 @@ attr_reader :price, :address, :rooms, :organize, :total_area, :details
       @total_area += totes_area.find_area
     end
     return @total_area
+  end
+
+  def price_per_square_foot
+    area
+    (@price.to_i / @total_area.to_f).round(2)
+  end
+
+  def rooms_sorted_by_area
+    @rooms.sort_by { |rm| -rm.find_area}
   end
 
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -58,4 +58,31 @@ describe House do
     expect(house.area).to eq(1900)
     expect(house.details).to eq({"price" => 400000, "address" => "123 Sugar Lane"})
   end
+
+  it "can be listed part 2" do
+    house = House.new("400000", "123 Sugar Lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, "15")
+    room_4 = Room.new(:basement, 30, "41")
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    expect(house.price_per_square_foot).to eq(210.53)
+  end
+
+    it "can be organized part 2" do
+      house = House.new("400000", "123 Sugar Lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, "15")
+      room_4 = Room.new(:basement, 30, "41")
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_sorted_by_area).to eq([room_4, room_3, room_2, room_1])
+    end
+
 end
