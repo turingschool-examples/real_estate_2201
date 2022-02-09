5d35d213c80764e5a7e14518217e0ecf27d52b2a
Joseph Downs <jos.w.downs@gmail.com>
2022-02-09 09:25:30 -0700

    Add add_room method

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,15 @@
+require "./room"
+
 class House
-attr_reader :price, :address
+attr_reader :price, :address, :rooms
 
-def initialize(price, address)
-  @price = price
-  @address = address
-end
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
 
+  def add_room(room)
+    @rooms << room
+  end
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -12,5 +12,15 @@ describe House do
     house = House.new("$400000", "123 Sugar Lane")
     expect(house.price).to eq("$400000")
     expect(house.address).to eq("123 Sugar Lane")
+    expect(house.rooms).to eq([])
+  end
+
+  it "has rooms" do
+    house = House.new("$400000", "123 Sugar Lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    expect(house.rooms).to eq([room_1, room_2])
   end
 end
