bace4b6c540d5efaf4879f76f47db47830e4d8ee
Joseph Senecal <jasenecal2@gmail.com>
2022-02-09 09:21:15 -0700

    Test: add paint method tests for Room

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -7,16 +7,28 @@ RSpec.describe Room do
       expect(room).to be_an_instance_of(Room)
     end
 
-    xit "has a category" do
+    it "has a category" do
       room = Room.new(:bedroom, 10, '13')
       expect(room.category).to eq(:bedroom)
     end
 
-    xit "can get area" do
+    it "can get area" do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
     end
+
+    it "can tell if a room has been painted" do
+      room1 = Room.new(:bedroom, 10, '13')
+      room2 = Room.new(:living_room, 15, '12')
+      expect(room1.is_painted?).to eq(false)
+      expect(room2.is_painted?).to eq(false)
+    end
+
+    it "can paint a room" do
+      room1 = Room.new(:bedroom, 10, '13')
+      room1.paint
+      expect(room1.is_painted?).to eq(true)
   end
 end
