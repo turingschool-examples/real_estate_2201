748c2cc960a03e8e35b8d27642761be24c1e0823
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-09 08:35:38 -0800

    Test: made house address test and fix naming for house price and house address test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -3,9 +3,14 @@ require './lib/house'
 
 RSpec.describe House do
   context "Iteration 2" do
-    it "exists" do
+    it "house price is 400000" do
       house = House.new("$400000", "123 sugar lane")
       expect(house.price).to eq(400000)
     end
+  end
+
+  it "house address is 123 sugar lane" do
+    house = House.new("$400000", "123 sugar lane")
+    expect(house.address).to eq("123 sugar lane")
   end
 end
