b11bf9374293b8e241074cb67d8511f6300258fb
Susan Boyd <sueboyd922@gmail.com>
2022-02-09 12:39:55 -0500

    feat: create room class

---
+++ lib/room.rb
@@ -0,0 +1,23 @@
+class Room
+  attr_reader :category
+  
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @painted = false
+  end
+
+  def area
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    @painted
+  end
+
+  def paint
+    @painted = true
+  end
+
+end
