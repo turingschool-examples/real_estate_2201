ccd5a68fab7a476a477ae56e0ee099269ec47b7a
Joseph Senecal <jasenecal2@gmail.com>
2022-02-09 09:48:09 -0700

    Feature: add House class and methods

---
+++ lib/house.rb
@@ -0,0 +1,12 @@
+class House
+  attr_reader :address, :price, :rooms
+  def initialize (price, address)
+    @address = address
+    @price = price.slice(1, price.length-1).to_i
+    @rooms = []
+  end
+
+  def add_room (room_to_add)
+    @rooms << room_to_add
+  end
+end
