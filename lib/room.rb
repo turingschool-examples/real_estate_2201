21cd5475df2f7023744d3e7d8f3174f7461eb12b
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-09 09:39:12 -0700

    iteration one

---
+++ lib/room.rb
@@ -0,0 +1,22 @@
+class Room
+  attr_reader :category, :length, :width
+
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @paint = false
+  end
+
+  def area
+    @length.to_i * @width.to_i
+  end
+
+  def is_painted?
+    @paint
+  end
+
+    def paint
+      @paint = true
+    end
+end
