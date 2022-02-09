8e63340c9dd00d7d2a6c675493a378dc974005f5
“Brad <hutch13531@gmail.com>
2022-02-09 12:59:00 -0500

    added house class

---
+++ lib/house.rb
@@ -0,0 +1,13 @@
+require './lib/room'
+
+class House
+  attr_reader :price, :address,:rooms, :area
+
+  def initialize(price, address)
+    @house = house
+    @price = price
+    @address = address
+    @rooms = rooms
+  end
+
+end
