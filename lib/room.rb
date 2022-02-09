c99a84afd2ade00dea50dee29714a07cf7e47ec4
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-09 11:13:40 -0500

    Iteration 1 test pass.

--- lib/room.rb
+++ lib/room.rb
@@ -1,14 +1,24 @@
 class Room
-  attr_reader :category, :length, :width
+  attr_reader :category, :length, :width, :has_paint
     
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
+    @has_paint = false
   end
 
   def area
     return length * width.to_i
   end
+
+  def is_painted?
+    return has_paint
+  end
+
+  def paint
+    @has_paint = true
+  end
+
     
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -18,5 +18,14 @@ RSpec.describe Room do
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
     end
+
+    it "can paint the room" do
+      room = Room.new(:bedroom, 10, '13')
+
+      expect(room.is_painted?).to eq(false)
+      room.paint
+      expect(room.is_painted?).to eq(true)
+    end
+
   end
 end
