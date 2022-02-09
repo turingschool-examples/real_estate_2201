bb0135a8ae3c7d64441e7aff42ad0f9a656f791b
Joseph Senecal <jasenecal2@gmail.com>
2022-02-09 10:00:57 -0700

    Test: add tests to house for listing room categories, area and details

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -25,6 +25,57 @@ RSpec.describe House do
 
       expect(house.rooms).to eq([room_1, room_2])
     end
+  end
+
+  context "Iteration 3" do
+    it 'can determine if value is over market average (fixed $500K)' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.above_market_average?).to eq(false)
+    end
+
+    xit 'can list all rooms of a given category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room2])
+      expect(house.rooms_from_category(:basement)).to eq([room_4])
+    end
+
+    xit 'can return the area of the entire house' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.area).to eq(1900)
+    end
+
+    it 'can return basic details as a hash' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+    end
 
   end
 end
