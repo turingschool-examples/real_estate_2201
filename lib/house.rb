5e60fe3d2bc4843b292327376000fb8a90357ebe
Joseph Downs <jos.w.downs@gmail.com>
2022-02-09 09:41:58 -0700

    Add rooms_from_category

--- lib/house.rb
+++ lib/house.rb
@@ -7,6 +7,7 @@ attr_reader :price, :address, :rooms
     @price = price
     @address = address
     @rooms = []
+    @organize = []
   end
 
   def add_room(room)
@@ -20,4 +21,16 @@ attr_reader :price, :address, :rooms
       false
     end
   end
+
+  def rooms_from_category(gory)
+    @rooms.each do |cat|
+      if gory == cat.category
+        @organize << cat
+      else
+      end
+    end
+    return @organize
+    @organize = []
+  end
+
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -27,6 +27,18 @@ describe House do
   it "is affordable" do
     house = House.new("$400000", "123 Sugar Lane")
     expect(house.above_market_average).to eq(false)
+  end
 
+  it "can be organized" do
+    house = House.new("$400000", "123 Sugar Lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, "15")
+    room_4 = Room.new(:basement, 30, "41")
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
   end
 end
