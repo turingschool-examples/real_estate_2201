13e870336a81aba42cf0f2af7a56cecac81c0667
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-09 09:57:57 -0800

    Feat: fix above market average, made rooms_from_category pass

--- lib/house.rb
+++ lib/house.rb
@@ -1,11 +1,12 @@
 class House
 
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :above_market_average
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @above_market_average = false
   end
 
   def price
@@ -16,4 +17,25 @@ class House
   def add_room(rooms)
     @rooms << rooms
   end
+
+  def above_market_average? #(has to false, less than 500000)
+    @price.delete("$").to_i > 500000
+  end
+
+  def rooms_from_category(category)
+    rooms = []
+    @rooms.each do |room|
+      if room.category == category
+        rooms << room
+      end
+    end
+    rooms
+  end
+
+  def area
+
+  end 
+
+
+
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -7,26 +7,68 @@ RSpec.describe House do
       house = House.new("$400000", "123 sugar lane")
       expect(house.price).to eq(400000)
     end
+
+
+    it "house address is 123 sugar lane" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.address).to eq("123 sugar lane")
+    end
+
+
+    it "rooms is an empty array" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.rooms).to eq([])
+    end
+
+    it "house has 2 rooms" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      expect(house.rooms).to eq([room_1, room_2])
+    end
   end
 
-  it "house address is 123 sugar lane" do
-    house = House.new("$400000", "123 sugar lane")
-    expect(house.address).to eq("123 sugar lane")
+  context "Iteration 3" do
+    it "house is above market average" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.above_market_average?).to eq(false)
+    end
+
+
+    it "house is above market average" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+      expect(house.rooms_from_category(:basement)).to eq([room_4])
+    end
+
+    it "house area" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.area).to eq(1900)
+    end
+
+
+
+
+
+
+
   end
-
-
-  it "rooms is an empty array" do
-    house = House.new("$400000", "123 sugar lane")
-    expect(house.rooms).to eq([])
-  end
-
-  it "house has 2 rooms" do
-    house = House.new("$400000", "123 sugar lane")
-    room_1 = Room.new(:bedroom, 10, '13')
-    room_2 = Room.new(:bedroom, 11, '15')
-    house.add_room(room_1)
-    house.add_room(room_2)
-    expect(house.rooms).to eq([room_1, room_2])
-  end
-
 end
