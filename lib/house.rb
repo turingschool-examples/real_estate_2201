c99bfacc2dd9c6ee75d85b9cc2fcf9e24b58972e
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-09 10:13:08 -0700

    Test: can determine if price is above market range, update iteration 2 tests for correct price format (integer) - Feat: above_market_average?

--- lib/house.rb
+++ lib/house.rb
@@ -2,12 +2,20 @@ class House
   attr_reader :price, :address, :rooms
 
   def initialize(price, address)
-    @price = price
+    @price = price.gsub(/\D/,'').to_i
     @address = address
     @rooms = []
   end
 
   def add_room(room_num)
     @rooms << (room_num)
+  end
+
+  def above_market_average?
+    if @price > 500000
+      return true
+    elsif @price < 500000
+      return false
+    end
   end
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -4,33 +4,39 @@ require './lib/house.rb'
 RSpec.describe House do
   context 'Iteration 2' do
     it 'exists' do
-      house = House.new('400000', '123 sugar lane')
+      house = House.new('$400000', '123 sugar lane')
       expect(house).to be_an_instance_of(House)
     end
 
     it 'has a price' do
-      house = House.new('400000', '123 sugar lane')
-      expect(house.price).to eq('400000')
+      house = House.new('$400000', '123 sugar lane')
+      expect(house.price).to eq(400000)
     end
 
     it 'has an address' do
-      house = House.new('400000', '123 sugar lane')
+      house = House.new('$400000', '123 sugar lane')
       expect(house.address).to eq('123 sugar lane')
     end
 
     it 'has a list of rooms' do
-      house = House.new('400000', '123 sugar lane')
+      house = House.new('$400000', '123 sugar lane')
       expect(house.rooms).to eq([])
     end
 
     it 'can add rooms' do
       room_1 = Room.new(:bedroom, 10, '13')
       room_2 = Room.new(:bedroom, 11, '15')
-      house = House.new('400000', '123 sugar lane')
+      house = House.new('$400000', '123 sugar lane')
       house.add_room(room_1)
       house.add_room(room_2)
 
       expect(house.rooms).to include(room_1, room_2)
+    end
+  end
+  context 'Iteration 3' do
+    it 'can determine if price is above market average' do
+      house = House.new('$400000', '123 sugar lane')
+      expect(house.above_market_average?).to eq(false)
     end
   end
 end
