800c6e6f2cb98a8b54b57a4c3932b531e85d9e77
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-09 08:21:04 -0800

    Feat: add is painted method

--- lib/room.rb
+++ lib/room.rb
@@ -1,15 +1,20 @@
 class Room
 
-attr_reader :category, :length, :width
+attr_reader :category, :length, :width, :is_painted
 
 def initialize(category, length, width )
   @category = category
   @length = length
   @width = width
+  @is_painted = false
 end
 
   def area
     @length * @width.to_i
+  end
+
+  def is_painted?
+    @is_painted
   end
 
 end
