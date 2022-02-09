ade19e1ffef5ebd5624a5326960d2537ec9b8ed9
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-09 09:50:55 -0700

    Test: can add rooms

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -18,5 +18,16 @@ RSpec.describe House do
       expect(house.address).to eq('123 sugar lane')
     end
 
+    it 'has a list of rooms' do
+      house = House.new('400000', '123 sugar lane')
+      expect(house.rooms).to eq([])
+    end
+
+    it 'can add rooms' do
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      house = House.new('400000', '123 sugar lane')
+      expect(house.rooms).to eq([room1, room2])
+    end
   end
 end
